a45f0a2c60322a7b58a4409927de9b32f5cab1c8
Ionut Staicu <iamntz@users.noreply.github.com>
2017-07-31 16:08:52 +0300

    ES6 update
    updated to es6, added loop & volume options.

--- ntzYoutubeEmbed.js
+++ ntzYoutubeEmbed.js
@@ -1,8 +1,7 @@
-;(function( $, document ){
+(function($, document) {
   /*
     <div class="item-type-video" data-youtube-id="YOUTUBE-VIDEO-ID"></div>
    */
-  if (!Object.create) { Object.create = function (o) { if (arguments.length > 1) { throw new Error('Object.create implementation only accepts the first parameter.'); } function F() {} F.prototype = o; return new F(); }; }
 
   var youtubeTag = document.createElement('script');
   youtubeTag.src = "//www.youtube.com/iframe_api";
@@ -10,104 +9,116 @@
   firstScriptTag.parentNode.insertBefore(youtubeTag, firstScriptTag);
 
   var ntzYoutubeEmbed = {
-    init: function( el ){
-      var $this = this;
-      $this.el = $( el );
+    init: function(el) {
+      this.el = $(el);
 
-      var ytTimer = window.setInterval(function(){
-        if( typeof YT == 'object' && typeof YT.Player == 'function' ){
-          window.clearInterval( ytTimer );
-          $this.YTApiReady();
+      var ytTimer = window.setInterval(() => {
+        if(typeof YT == 'object' && typeof YT.Player == 'function') {
+          window.clearInterval(ytTimer);
+          this.YTApiReady();
         }
       }, 10);
-    } // init
+    },
 
 
-    ,YTApiReady: function(){
-      var $this = this;
-      this.movieContainerID = 'video-' + Math.round( Math.random() * 1000000 );
+    YTApiReady: function() {
+      this.movieContainerID = 'video-' + Math.round(Math.random() * 1000000);
 
       $('<div class="youtube-iframe" />').attr({
         'id': this.movieContainerID
-      }).appendTo( $this.el );
+      }).appendTo(this.el);
 
-      $this.createPlayer();
-      
-      this.el.on('player-pause', function(){ $this.player.pauseVideo(); });
-      this.el.on('player-stop', function(){ $this.player.seekTo(0); $this.player.stopVideo(); });
-      this.el.on('player-play', function(){ $this.player.playVideo(); });
+      this.createPlayer();
 
-      this.el.on('player-toggle-state', function(){
-        var player = $this.player,
-            state = player.getPlayerState();
+      this.el.on('player-pause', () => {
+        this.player.pauseVideo();
+      });
 
-        if( state == 1 ){
-          player.pauseVideo();
+      this.el.on('player-stop', () => {
+        this.player.seekTo(0);
+        this.player.stopVideo();
+      });
+
+      this.el.on('player-play', () => {
+        this.player.playVideo();
+      });
+
+      this.el.on('player-toggle-state', () => {
+        let state = this.player.getPlayerState();
+
+        if(state == YT.PlayerState.PLAYING) {
+          this.player.pauseVideo();
         }
 
-        if( state == 2 ){
-          player.playVideo();
+        if(state == YT.PlayerState.PAUSED) {
+          this.player.playVideo();
         }
       });
-    }//YTApiReady
+    },
 
-
-    ,createPlayer: function(){
-      var $this = this;
-
+    createPlayer: function() {
       var playerVars = $.extend({
-          enablejsapi   : 1,
-          autohide      : 2,
-          iv_load_policy: 3,
-          modestbranding: true,
-          origin        : window.location.origin,
-          rel           : 0,
-          showinfo      : 0,
-          theme         : 'light',
-          color         : 'white',
-          controls      : 0,
-          autoplay      : 0,
-          poster        : ""
-        }, $this.el.data() );
+        enablejsapi: 1,
+        autohide: 2,
+        iv_load_policy: 3,
+        modestbranding: true,
+        origin: window.location.origin,
+        rel: 0,
+        showinfo: 0,
+        theme: 'light',
+        color: 'white',
+        controls: 0,
+        autoplay: 0,
+        volume: 100,
+        loop: 0,
+        poster: ""
+      }, this.el.data());
 
-      if( playerVars.poster ){
-        var poster = $('<img class="player-poster" />').attr('src', playerVars.poster );
-        poster.appendTo( $this.el );
-        $this.el.addClass('player-has-poster');
-        poster.on('click', function(){
+      this.playerVars = playerVars;
+
+      if(playerVars.poster) {
+        var poster = $('<img class="player-poster" />').attr('src', playerVars.poster);
+        poster.appendTo(this.el);
+        this.el.addClass('player-has-poster');
+        poster.on('click', function() {
           $(this).addClass('is-playing');
-          $this.el.trigger('player-play');
+          this.el.trigger('player-play');
         });
       }
 
-      this.player = new YT.Player( this.movieContainerID, {
-        height    : '100%',
-        width     : '100%',
-        videoId   : this.el.data('youtube-id'),
+      this.player = new YT.Player(this.movieContainerID, {
+        height: '100%',
+        width: '100%',
+        volume: 0,
+        videoId: this.el.data('youtube-id'),
         playerVars: playerVars,
-        events    : {
-          'onReady'      : $.proxy( $this.movieReady, $this ),
-          'onStateChange': $.proxy( $this.stateChange, $this )
+        events: {
+          'onReady': $.proxy(this.movieReady, this),
+          'onStateChange': $.proxy(this.stateChange, this)
         }
       });
-    }//createPlayer
+    },
 
 
-    ,movieReady: function( player ){
-      this.el.data('youtubePlayer', player.target ).trigger( 'player-ready', player );
-    }//movieReady
+    movieReady: function(player) {
+      player.target.setVolume(this.playerVars.volume);
+      this.el.data('youtubePlayer', player.target).trigger('player-ready', player);
+    },
 
 
-    ,stateChange: function( state ){
+    stateChange: function(state) {
+      if (this.playerVars.loop && state.data === YT.PlayerState.ENDED) {
+        this.player.playVideo();
+      }
       this.el.data('video-state', state).trigger('player-state-change', state);
-    }//stateChange
+    } //stateChange
   };
 
 
   $.fn.ntzYoutubeEmbed = function() {
-    return this.each(function(){
-      var obj = Object.create( ntzYoutubeEmbed );
-      obj.init( this );
+    return this.each(function() {
+      var obj = Object.create(ntzYoutubeEmbed);
+      obj.init(this);
     });
   };
-})( jQuery, document );
+})(jQuery, document);
